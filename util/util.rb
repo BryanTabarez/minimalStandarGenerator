a20f5bc4dff0d85df5f18454f8df9042598c28ba
BryanTabarez <userbryan@gmail.com>
2017-03-11 03:58:00 +0000

    Revert "Revert "Revert "haciendo pruebas en git"""
    This reverts commit 41fd7e6109b8f852d61e1b52f6932fd63756b28c.

--- util/util.rb
+++
@@ -1,9 +0,0 @@
-class Util
-    def self.generateStringData(genData, ndecimals)
-        dataStr = Array.new
-        genData.each do |number|
-            dataStr << "%.#{ndecimals}f" % number
-        end
-        dataStr
-    end
-end
